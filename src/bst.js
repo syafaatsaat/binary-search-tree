5f29d5623c63854d78288a6b925df872543408df
Syafaat Saat <47024629+syafaatsaat@users.noreply.github.com>
2026-05-28 16:03:44 +0800

    Create BST and function to build tree from array

---
+++ src/bst.js
@@ -0,0 +1,59 @@
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class Tree {
+  constructor(array) {
+    this.root = null;
+    this.#buildTree(array);
+  }
+
+  #buildTree(array) {
+    const sortedUniqueArr = [...new Set(array)].sort();
+    this.root = this.#buildTreeRecursive(
+      sortedUniqueArr, 0, sortedUniqueArr.length
+    );
+  }
+
+  #buildTreeRecursive(array, start, end) {
+    if (start > end) {
+      return null;
+    }
+
+    const mid = start + (end - start) / 2;
+    const root = new Node(array[mid]);
+
+    root.left = this.#buildTreeRecursive(array, start, mid-1);
+    root.right = this.#buildTreeRecursive(array, mid+1, end);
+
+    return root;
+  }
+
+  #prettyPrint(node, prefix = '', isLeft = true) {
+    if (node === null || node === undefined) {
+      return;
+    }
+
+    this.#prettyPrint(
+      node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false
+    );
+    console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+    this.#prettyPrint(
+      node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true
+    );
+  }
+
+  printTree() {
+    this.#prettyPrint(this.root);
+  }
+
+  includes(value) {
+    
+  }
+}
+
+
